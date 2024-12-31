b9cb4631873d0becc71c4be74393d0b81c75cc5e
i-brkic <184224468+i-brkic@users.noreply.github.com>
2024-12-31 13:28:00 +0100

    Set up a hover effect
    Add a hover effect which leaves a pixelated
    trail by changing a square color when the mouse
    enters the square and leaves the same color when
    the mouse leaves it.

--- script.js
+++ script.js
@@ -4,11 +4,16 @@ for (let i = 1; i <= 16; i++) {
   const row = document.createElement("div");
   row.style.display = "flex";
   container.appendChild(row);
+
   for (let j = 1; j <= 16; j++) {
     const gridSquare = document.createElement("div");
     gridSquare.style.border = "1px solid black";
     gridSquare.style.height = "5vh";
     gridSquare.style.width = "5vw";
     row.appendChild(gridSquare);
+
+    gridSquare.addEventListener("mouseover", function () {
+      gridSquare.style.backgroundColor = "red";
+    });
   }
 }
